e49cb02687169e5f2f68270a3a8f0ce8315d3a71
Nam <hibou107@gmail.com>
2016-02-02 00:02:40 +0100

    test foldMap

--- src/main/scala/monoid/Monoid.scala
+++ src/main/scala/monoid/Monoid.scala
@@ -80,15 +80,27 @@ object Toto {
     println(result)
   }
 
+  // Exercice 10.10
+
   sealed trait WC
   case class Stub(chars: String) extends WC
   case class Part(lStub: String, words: Int, rStub: String) extends WC
   val wcMonoid: Monoid[WC] = new Monoid[WC]{
     def op(a1: WC, a2: WC) = {
-      case
+      (a1, a2) match {
+        case (Stub(c), Stub(d)) => Stub(c + d)
+        case (Stub(c), Part(l, w, r)) => Part(c + l, w, r)
+        case (Part(l, w, r), Stub(c)) => Part(l, w, r + c)
+        case (Part(l1, w1, r1), Part(l2, w2, r2)) => Part(l1, w1 + (if ((r1 + l2).isEmpty) 0 else 1) + w2, l2)
+      }
     }
+    def zero = Stub("")
   }
 
+  // Exercice 10.11
+  def count(a: String): Int = {
 
 
+    ???
+  }
 }
