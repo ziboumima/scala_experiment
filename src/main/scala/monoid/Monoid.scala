cc1b164c4b4b9df37c929674bd4a012a7cbf055a
Nam <hibou107@gmail.com>
2016-02-02 21:47:55 +0100

    count and test count

--- src/main/scala/monoid/Monoid.scala
+++ src/main/scala/monoid/Monoid.scala
@@ -98,8 +98,19 @@ object Toto {
   }
 
   // Exercice 10.11
-  def count(a: String): Int = {
-
-    ???
+  def count(s: String): Int = {
+    // A single character's count. Whitespace does not count,
+    // and non-whitespace starts a new Stub.
+    def wc(c: Char): WC =
+      if (c.isWhitespace)
+        Part("", 0, "")
+      else
+        Stub(c.toString)
+    // `unstub(s)` is 0 if `s` is empty, otherwise 1.
+    def unstub(s: String) = s.length min 1
+    foldMapV(s.toIndexedSeq, wcMonoid)(wc) match {
+      case Stub(s) => unstub(s)
+      case Part(l, w, r) => unstub(l) + w + unstub(r)
+    }
   }
 }
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,8 +1,12 @@
+import monoid.Toto
 import org.scalatest._
 import org.scalatest.matchers.ShouldMatchers
 
 class HelloSpec extends FlatSpec with ShouldMatchers {
   "Hello" should "have tests" in {
     true should be === true
+  }
+  "Count" should "do the job" in {
+    Toto.count("this should be good") should be === 4
   }
 }
