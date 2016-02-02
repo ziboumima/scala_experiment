a4ceb267495896272f5dca402d7b86a99971ee05
Nam <hibou107@gmail.com>
2016-02-02 22:08:44 +0100

    start list foldable

---
+++ src/main/scala/fondable/Fondable.scala
@@ -0,0 +1,22 @@
+package fondable
+
+import monoid.Monoid
+
+/**
+  * Created by hibou on 02/02/16.
+  */
+
+trait Foldable[F[_]] {
+  def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B
+  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B
+  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B
+  def concatenate[A](as: F[A])(m: Monoid[A]): A = foldLeft(as)(m.zero)(m.op)
+}
+
+object ListFoldable extends Foldable[List]{
+  override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = as.foldRight(z)(f)
+
+  override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = ???
+
+  override def foldMap[A, B](as: List[A])(f: (A) => B)(mb: Monoid[B]): B = ???
+}
