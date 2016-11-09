e79ed653619c15441ebb481ae0085c593487d26e
nnguyen <nam.nguyen@the-ica.com>
2016-11-09 17:35:59 +0100

    Free

--- src/main/scala/regex/Demo.scala
+++ src/main/scala/regex/Demo.scala
@@ -5,11 +5,15 @@ package regex
 object Demo {
   def main(args: Array[String]) {
     val pattern = "\\A(S|s)cala"
-    val datePattern = "\\A\\d{2}\\.\\d{2}\\.\\d{4}"
+    val datePattern = "(\\A\\d{2}\\.\\d{2}\\.\\d{4})"
     val periodPattern = "\\d{1,2}[DMY]"
     val rangePattern = s"((?:$periodPattern)|(?:$datePattern)):((?:$periodPattern)|(?:$datePattern)):($periodPattern)"
     val str = "Scala is scalable and cool"
     val date = "01.02.2015"
+
+    date match {
+      case datePattern.r(x) => print(x)
+    }
 
     println(datePattern.r.unapplySeq(date).isDefined)
     println(periodPattern.r.unapplySeq("12Y").isDefined)
